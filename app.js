8ec097b48b9a3a6b90080118cb1d46b208cb7bc5
Mofaz Shmuely <mofazshmuely69@gmail.com>
2023-01-29 19:17:32 +0200

    basic server

---
+++ app.js
@@ -0,0 +1,35 @@
+// require("dotenv").config();
+const express = require("express");
+// const path = require("path");
+const cors = require("cors");
+// const mongoose = require("mongoose");
+
+// const { userRouter } = require("./routes/user-routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // handle requests that have Content-Type: application/json
+app.use(express.urlencoded({ extended: true })); // handles parsing of forms
+
+// localhost:3001/a.txt
+// app.use(express.static(path.join(__dirname, "static")));
+
+// ROUTERS
+// app.use("/user", userRouter);
+
+// mongoose.set("strictQuery", true);
+// mongoose.connection.on("error", (err) => console.error(err));
+// mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
+// mongoose.connect(process.env.MONGO_URI);
+
+const PORT = 3000;
+// process.env.STATUS === "production"
+//   ? process.env.PROD_PORT
+//   : process.env.DEV_PORT;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server in ${process.env.STATUS} mode, listening on port: ${PORT}`
+  );
+});
