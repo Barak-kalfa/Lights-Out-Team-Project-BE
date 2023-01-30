659e3474325bb15f19a96869644b5dc7a69e4913
Barak Kalfa <BarakK@cnoga.local>
2023-01-30 18:36:02 +0200

    logic

---
+++ GameLogic/GameLogic.js
@@ -0,0 +1,15 @@
+
+function randomBoolean(){
+     const num = Math.random();
+     return num > 0.5 ? true : false;
+}
+
+function startGame(){
+     
+}
+
+function handleClick({userName, lightId}){
+
+}
+
+module.exports = {randomBoolean}
